3af263ef7bcacb0b993ee520e55f3d5682124d88
Yash Mehta <67110594+yashmehta17@users.noreply.github.com>
2020-10-02 11:36:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pyttsx3 as ts
 book = open('C:/Users/GAGAN CHORDIA/Desktop/JavaDA.pdf','rb')#between the first quotes add the location of the pdf file
 reader = PyPDF2.PdfFileReader(book)
 pages = reader.numPages
-page = reader.getPage(8)
-text = page.extractText()
-x=ts.init()
-x.say(text)
-x.runAndWait()
+for num in range(0,pages):
+    page = reader.getPage(num)
+    text = page.extractText()
+    x=ts.init()
+    x.say(text)
+    x.runAndWait()
